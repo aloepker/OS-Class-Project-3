81dea1a750e2ca9099ef7cf2f1d6327cbd3a8fc0
aloepker <aloepker@gmail.com>
2023-10-07 20:43:07 -0500

    Figured out code problems from project 2 thanks to professor Hauschild. Also got message queue working! currently just sending messages from the parent to the child. Next up, to practice sending messages back from the child and implement more of the project code. Getting there!

--- oss.c
+++ oss.c
@@ -8,6 +8,8 @@
 #include <sys/shm.h>
 #include <string.h>
 #include <time.h>
+#include <sys/msg.h>
+#include <errno.h>
 
 int sysClockNano = 0;
 int sysClockSec = 0;
@@ -42,6 +44,13 @@ struct PCB {
 	int startNano;       //time when it was forked
 };
 struct PCB processTable[20];
+#define PERMS 0777
+typedef struct msgbuffer {
+	long mtype;
+	char strData[100];
+	int intData;
+} msgbuffer;
+
 int main(int argc, char** argv){
 	int option, status;
 	int numWorkers = 0;
@@ -78,15 +87,11 @@ int main(int argc, char** argv){
 				help();
 				return EXIT_SUCCESS;
 		}
-	}
-//open file to print to :
-	
-
-
+	}	
 	//initializing shared memor
 	int shmid = shmget ( SHMKEY, BUFF_SZ, 0777 | IPC_CREAT );
 	if ( shmid == -1 ){
-		perror("Shared Memory Creation Error!!!");
+		perror("Shared Memory Creation Error!!!\n");
 		return EXIT_FAILURE;
 	}
 	char * paddr = ( char * )( shmat ( shmid, 0, 0 ) );
@@ -94,8 +99,24 @@ int main(int argc, char** argv){
 	shmTime[0]=123;
 	shmTime[1]=123;
  	shmdt(shmTime);
+//message que initial implementation:
+	msgbuffer buf0, buf1;
+	int msqid;
+	key_t key;
+	system("touch msgq.txt");
+	//sets message queue key
+	if ((key = ftok("msgq.txt", 1)) == -1) {
+		perror("parent ftok error");
+		exit(1);
+	}
+	//creates message queue
+	if ((msqid = msgget(key, PERMS | IPC_CREAT)) == -1) {
+		perror("msgget in parent error");
+		exit(1);
+	}
+	printf("Message queue has been set up successfully!\n");
 
-//	printf("%s",logFile);
+		
 
 	printf("Number of workers Selected: %d\nNumber of Workers at a time: %d\nNumber of loops for each Worker: %d\nOutput file: %s\n", numWorkers, workerLimit, timeLimit,logFile);
 //	int i=0,j=0;
@@ -137,22 +158,43 @@ int main(int argc, char** argv){
 			execvp("./worker", args);
 		}
 //	}//  for loop
+
+	// here i am no longere in the child side of the fork, so I could in theory add to a process table here
+
+
+
+
 	//parent side of fork if
+
 	if(childPid != 0) {
-	int statusPid;
-	//open output file
-	FILE *outputFile = fopen(logFile, "w");
-	if (outputFile==NULL){
-	printf("Error opening output file!\nTerminating program\n");
-	return EXIT_FAILURE;
-	}
-	//parent shared memory
+		int statusPid;
+		//open output file
+		FILE *outputFile = fopen(logFile, "w"); //there is also "a" for apend and "r" for read
+		if (outputFile==NULL){
+			printf("Error opening output file!\nTerminating program\n");
+			return EXIT_FAILURE;
+		}
+		// now to test adding pid to the output file
+		fprintf(outputFile, "Child's PID: %d\n",childPid);// it works!
+		//parent shared memory
 		int shmidp = shmget(SHMKEY, BUFF_SZ, 0777);
 		if (shmidp == -1){
 			printf("Parent process shared memory error!\n");
 			return EXIT_FAILURE;
 		}
 		int * ppint = (int*)(shmat(shmidp,0,0));
+		//shared memory implemented
+//now lets try adding a message for the que here to start
+		buf1.mtype = childPid;
+		buf1.intData = childPid;
+		strcpy(buf1.strData, "Message to Child form Parent");
+		
+		if (msgsnd(msqid, &buf1, sizeof(msgbuffer)-sizeof(long), 0) == -1) {
+			perror("msgsnd to child failed");
+		}
+
+
+
 		int nanoFlag = 0;
 //			while (j < numWorkers) {
 				incrementClock();
@@ -173,8 +215,15 @@ int main(int argc, char** argv){
 					//print and set nano flag
 //					nanoFlag = 1;
 //			}
-	shmdt(ppint);
-	shmctl(shmid, IPC_RMID, NULL);
-printf("shared memory shutdown successful\n");
+		shmdt(ppint);
+		shmctl(shmid, IPC_RMID, NULL);
+		printf("shared memory shutdown successful\n");
+		fclose(outputFile);
+//also clear message ques:
+		if (msgctl(msqid, IPC_RMID, NULL) == -1){
+			perror("msgctl to get rid of que in parent ");
+			exit(1);
+		}
+	
 	}//end of parent side of fork if
 }
--- worker.c
+++ worker.c
@@ -4,19 +4,50 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <signal.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+
 #define SHMKEY 859048
 #define BUFF_SZ sizeof ( int )
+#define PERMS 0777
+typedef struct msgbuffer {
+	long mtype;
+	char strData[100];
+	int intData;
+} msgbuffer;
+
+
 void signal_handler(int signum){
-printf("60 SecondTimelimit Reached! Terminating Worker Program\n");
+printf("60 Second time limit Reached! Terminating Worker Program\n");
 exit(0);
 }
+
 int main(int argc, char** argv){
 	signal(SIGALRM, signal_handler);
 	alarm(3);
+
+	msgbuffer buf;
+	buf.mtype = 1;
+	int msqid = 0;
+	key_t key;
+	// aquire key for message queue
+	if ((key = ftok("msgq.txt", 1)) == -1) {
+		perror("child ftok error");
+		exit(1);
+	}
+	// create message queue
+	if ((msqid = msgget(key, PERMS)) == -1) {
+		perror("msgget error in child"); // current error is before here
+		exit(1);
+	}
+	printf("Child has access to the message que!\n");
+
 	if(argc>2){
 		int shmidc = shmget(SHMKEY, BUFF_SZ, 0777);
 		if (shmidc == -1){
-			printf("Child process shared memory error!");
+			perror("Child process shared memory error!");
 			return EXIT_FAILURE;
 		}
 		int * cint= (int*)(shmat(shmidc,0,0));
@@ -28,7 +59,18 @@ int main(int argc, char** argv){
 		int startSec = cint[0];
 		int secActive = 0;
 		printf("WORKER PID: %d PPID %d SysClockS: %d SysclockNano %d TermTimeS: %d TermTimeNano: %d --Just Starting\n",getpid(),getppid(),cint[0], cint[1], timeoutSec, timeoutNano);
+		//loop that checks the clock:
 		while(timeUp != 1){
+			
+			// message queue read test:
+			if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) {
+				perror("failed to recieve message form parent");
+				exit(1);
+			}
+			//printing the recieved message test:
+			printf("Child %d recieved message: %d\n", getpid(), buf.intData);
+
+
 			if ( secActive < (cint[0]-startSec) ){
 				secActive++;
 				printf("WORKER PID: %d PPID %d SysClockS: %d SysclockNano %d TermTimeS: %d TermTimeNano: %d -- %d seconds have passed since starting\n",getpid(),getppid(),cint[0], cint[1], timeoutSec, timeoutNano, secActive);
